d61100f1ff01fbd037cf59b19bccd9c19773619c
aesty1 <kadirovdenis123@gmail.com>
2025-08-25 09:23:32 +0300

    first commit

---
+++ src/main/java/ru/denis/aestymes/AestyMessengerApplication.java
@@ -0,0 +1,13 @@
+package ru.denis.aestymes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AestyMessengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AestyMessengerApplication.class, args);
+    }
+
+}
